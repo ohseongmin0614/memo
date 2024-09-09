0629a09c5e50563cbd4a22ba9ea65f0fec8eee16
ohseongmin0614 <47583316+ohseongmin0614@users.noreply.github.com>
2024-09-09 23:54:53 +0900

    memoapp
    memoapp 입력, 수성, 삭제

--- main.py
+++ main.py
@@ -14,10 +14,28 @@ app = FastAPI()
 @app.post("/memos")
 def create_memo(memo:Memo):
     memos.append(memo)
-    return '메모추가완료'
+    return '메모 추가 완료'
 
 @app.get("/memos")
 def read_memo():
     return memos
+
+@app.put("/memos/{memo_id}")
+def put_memo(req_memo:Memo):
+    for memo in memos:
+        if memo.id==req_memo.id:
+            memo.content = req_memo.content
+            return'메모 수정 완료'
+    return'없는 메모입니다.'
+
+@app.delete("/memos/{memo_id}")
+def delete_memo(memo_id):
+    for index,memo in enumerate(memos):
+         if memo.id==memo_id:
+            memos.pop(index)
+            return'메모 삭제 완료'
+    return'없는 메모입니다.'
+
+
 
 app.mount("/", StaticFiles(directory="static",html=True), name="static")
--- static/app.js
+++ static/app.js
@@ -1,10 +1,55 @@
+async function editMemo(event) {
+  const id = event.target.dataset.id;
+  const editInput = prompt("수정할 값을 입력하세요");
+  const res = await fetch(`/memos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      //문자열만 통신이 가능하기때문에 JSON->문자열로 변환해서 송신
+      id,
+      content: editInput,
+    }),
+  });
+  readMemo();
+}
+
+async function delMemo(event) {
+  const id = event.target.dataset.id;
+  const res = await fetch(`/memos/${id}`, {
+    method: "DELETE",
+  });
+  readMemo();
+}
+
+function displayMemos(memo) {
+  const ul = document.querySelector("#memo-ul");
+  const li = document.createElement("li");
+  li.innerText = `[id:${memo.id}] ${memo.content}`;
+
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "수정하기";
+  editBtn.addEventListener("click", editMemo);
+  editBtn.dataset.id = memo.id;
+
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "삭제";
+  delBtn.addEventListener("click", delMemo);
+  delBtn.dataset.id = memo.id;
+
+  li.appendChild(editBtn);
+  li.appendChild(delBtn);
+  ul.appendChild(li);
+}
+
 async function readMemo() {
   const res = await fetch("/memos");
   const jsonRes = await res.json();
-  console.log(jsonRes);
+  const ul = document.querySelector("#memo-ul");
+  ul.innerHTML = "";
+  jsonRes.forEach(displayMemos);
 }
-
-readMemo();
 
 async function createMemo(value) {
   const res = await fetch("/memos", {
@@ -14,17 +59,19 @@ async function createMemo(value) {
     },
     body: JSON.stringify({
       //문자열만 통신이 가능하기때문에 JSON->문자열로 변환해서 송신
-      id: new Date().getTime(),
+      id: new Date().getTime().toString(),
       content: value,
     }),
   });
-  const jsonRes = await res.json();
+  readMemo();
 }
 function handleSubmit(event) {
-  event.preventDefault(); //submit에서 새로고침까지 진행해버리기에 이벤트를 막아서 log 출력.
+  event.preventDefault(); //submit에서 새로고침까지 진행해버리기에 이벤트를 막아서 동작하게 한다
   const input = document.querySelector("#memo-input");
   createMemo(input.value);
-  input.value = "";
+  input.value = " ";
 }
 const form = document.querySelector("#memo-form");
 form.addEventListener("submit", handleSubmit);
+
+readMemo();
